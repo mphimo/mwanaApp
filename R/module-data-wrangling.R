c2bbea3fb0e2ea1044e9d0a746e3d3b4c8c7ac0a
tomaszaba <tomas.zaba@outlook.com>
2025-12-04 06:07:16 +0200

    add a shiny module for data wrangling

---
+++ R/module-data-wrangling.R
@@ -0,0 +1,348 @@
+## ---- Module: UI -------------------------------------------------------------
+
+
+#'
+#'
+#'
+#' Module UI for data wrangling
+#'
+#' @param id Module ID
+#'
+#'
+#' @keywords internal
+#'
+#'
+#'
+module_ui_wrangling <- function(id) {
+  ## Namespace ID ----
+  ns <- shiny::NS(id)
+
+  ## UI elements ----
+  shiny::tagList(
+    bslib::layout_sidebar(
+      sidebar = bslib::sidebar(
+        width = 400,
+        style = "width: 400px;",
+
+        ### Left side of the nav panel ----
+        bslib::card(
+          style = "background-color: #f9fdfb;",
+          bslib::card_header(htmltools::tags$span("Define Parameters for Data Wrangling",
+            style = "font-size: 15px; font-weight: bold;"
+          )),
+
+          #### Display data wrangling method options ----
+          shiny::radioButtons(
+            inputId = ns("wrangle"),
+            label = htmltools::tags$span("Select Method",
+              style = "font-size: 14px; font-weight: bold;"
+            ),
+            choices = list(
+              "Weight-for-Height z-scores (WFHZ)" = "wfhz",
+              "MUAC-for-Age z-scores (MFAZ)" = "mfaz",
+              "Raw MUAC" = "muac",
+              "Combined (WFHZ and MFAZ)" = "combined"
+            ),
+            selected = "wfhz"
+          ),
+
+          #### Display variable inputs rendered from the server ----
+          shiny::uiOutput(outputId = ns("select_vars")),
+          htmltools::tags$br(),
+          shiny::actionButton(
+            inputId = ns("apply_wrangle"),
+            label = "Wrangle",
+            class = "btn-primary"
+          )
+        )
+      ),
+
+      ### Right side of the nav panel ----
+      bslib::card(
+        style = "background-color: #f9fdfb;",
+        bslib::card_header(htmltools::tags$span("Data Preview",
+          style = "font-size: 15px; font-weight: bold;"
+        )),
+
+        #### A Placehoder for wrangled data and embed user feedback ----
+        shinycssloaders::withSpinner(
+          ui_element = DT::DTOutput(outputId = ns("wrangled")),
+          type = 8,
+          color.background = "#004225",
+          image = "logo.png",
+          image.height = "50px",
+          color = "#004225",
+          caption = htmltools::tags$div(
+            htmltools::tags$h6(htmltools::tags$span("Wrangling",
+              style = "font-size: 12px;"
+            )),
+            htmltools::tags$h6(htmltools::tags$span("Please wait...",
+              style = "font-size: 12px;"
+            ))
+          )
+        ),
+
+        #### Placeholder for donwload button ----
+        shiny::uiOutput(outputId = ns("download_wrangled_data"))
+      )
+    )
+  )
+}
+
+
+## ---- Module: Server ---------------------------------------------------------
+
+
+#'
+#'
+#' Module server for data wrangling
+#'
+#' @param id Module ID
+#'
+#'
+#' @keywords internal
+#'
+#'
+#'
+module_server_wrangling <- function(id, data) {
+  shiny::moduleServer(
+    id,
+    function(input, output, session) {
+      ns <- session$ns
+
+      ### Capture reactivity ----
+      dataset <- shiny::reactiveValues(wrangled = NULL)
+
+      ### Fetch reactive user inputs ----
+      ui_inputs <- shiny::reactive({
+        shiny::req(data(), input$wrangle)
+
+        #### Get variables names to display inside input selectors ----
+        vars <- base::names(data())
+
+        #### Display variables based on the wrangling method ----
+        mod_data_wrangling_display_input_variables(
+          vars = vars, method = input$wrangle, ns = ns
+        )
+      })
+
+      ### Render variables dynamically ----
+      output$select_vars <- shiny::renderUI({
+        shiny::tagList(ui_inputs())
+      })
+
+      ### Create container for reactivity ----
+      dataset$wrangling <- shiny::reactiveVal(FALSE)
+
+      shiny::observeEvent(input$apply_wrangle, {
+        shiny::req(data())
+        dataset$wrangling(TRUE)
+
+        #### Handle errors gracefully ----
+        valid <- TRUE
+        message <- ""
+
+        if (input$wrangle == "wfhz") {
+          if (any(!nzchar(c(input$sex, input$weight, input$height)))) {
+            valid <- FALSE
+            message <- "Please select all required variables."
+          }
+        } else if (input$wrangle == "mfaz") {
+          if (any(!nzchar(c(input$age, input$sex, input$muac)))) {
+            valid <- FALSE
+            message <- "Please select all required variables."
+          }
+        } else if (input$wrangle == "muac") {
+          if (any(!nzchar(c(input$sex, input$muac)))) {
+            valid <- FALSE
+            message <- "Please select all required variables."
+          }
+        } else {
+          if (any(!nzchar(c(input$age, input$sex, input$weight, input$height, input$muac)))) {
+            valid <- FALSE
+            message <- "Please select all required variables."
+          }
+        }
+
+        if (!valid) {
+          shiny::showNotification(message, type = "error")
+          dataset$wrangling(FALSE)
+          return()
+        }
+
+        tryCatch(
+          expr = {
+            w <- switch(EXPR = input$wrangle,
+              "wfhz" = {
+                shiny::req(input$sex, input$weight, input$height)
+                data() |>
+                  dplyr::rename(
+                    sex = !!rlang::sym(input$sex),
+                    weight = !!rlang::sym(input$weight),
+                    height = !!rlang::sym(input$height)
+                  ) |>
+                  mw_wrangle_wfhz(
+                    sex = .data$sex,
+                    .recode_sex = TRUE,
+                    weight = .data$weight,
+                    height = .data$height
+                  )
+              },
+              "mfaz" = {
+                shiny::req(input$age, input$sex, input$muac)
+
+                data() |>
+                  dplyr::mutate(
+                    muac = recode_muac(x = !!rlang::sym(input$muac), .to = "cm")
+                  ) |>
+                  dplyr::rename(
+                    age = !!rlang::sym(input$age),
+                    sex = !!rlang::sym(input$sex)
+                  ) |>
+                  mw_wrangle_age(dos = NULL, dob = NULL, age = .data$age) |>
+                  mw_wrangle_muac(
+                    sex = .data$sex,
+                    .recode_sex = TRUE,
+                    age = .data$age,
+                    muac = .data$muac,
+                    .recode_muac = FALSE,
+                    .to = "none"
+                  )
+              },
+              "muac" = {
+                shiny::req(input$sex, input$muac)
+
+                data() |>
+                  dplyr::rename(
+                    sex = !!rlang::sym(input$sex),
+                    muac = !!rlang::sym(input$muac)
+                  ) |>
+                  mw_wrangle_muac(
+                    sex = .data$sex,
+                    .recode_sex = TRUE,
+                    age = NULL,
+                    muac = .data$muac,
+                    .recode_muac = FALSE,
+                    .to = "none"
+                  )
+              },
+              "combined" = {
+                shiny::req(
+                  input$age, input$sex, input$weight, input$height,
+                  input$muac
+                )
+
+                data() |>
+                  dplyr::mutate(
+                    muac = recode_muac(x = !!rlang::sym(input$muac), .to = "cm")
+                  ) |>
+                  dplyr::rename(
+                    age = !!rlang::sym(input$age),
+                    sex = !!rlang::sym(input$sex),
+                    weight = !!rlang::sym(input$weight),
+                    height = !!rlang::sym(input$height)
+                  ) |>
+                  mw_wrangle_wfhz(
+                    sex = .data$sex,
+                    .recode_sex = TRUE,
+                    weight = .data$weight,
+                    height = .data$height
+                  ) |>
+                  mw_wrangle_age(dos = NULL, dob = NULL, age = .data$age) |>
+                  mw_wrangle_muac(
+                    sex = .data$sex,
+                    .recode_sex = FALSE,
+                    age = .data$age,
+                    muac = .data$muac,
+                    .recode_muac = FALSE,
+                    .to = "none"
+                  )
+              }
+            )
+
+            dataset$wrangled <- w
+          },
+          error = function(e) {
+            shiny::showNotification(
+              paste("Wrangling error:", e$message),
+              type = "error"
+            )
+          }
+        )
+
+        dataset$wrangling(FALSE)
+      })
+
+      ### Display wrangled data ----
+      output$wrangled <- DT::renderDT({
+        shiny::req(!dataset$wrangling())
+        shiny::req(dataset$wrangled)
+
+        DT::datatable(
+          data = utils::head(dataset$wrangled, 20),
+          rownames = FALSE,
+          options = list(
+            pageLength = 20,
+            scrollX = FALSE,
+            scrolly = "800px",
+            columnDefs = list(list(className = "dt-center", targets = "_all"))
+          ),
+          caption = if (nrow(dataset$wrangled) > 20) {
+            paste(
+              "Showing first 20 rows of", format(nrow(dataset$wrangled), big.mark = ","),
+              "total rows"
+            )
+          } else {
+            paste("showing all", nrow(dataset$wrangled), "rows")
+          }
+        ) |> DT::formatStyle(columns = colnames(dataset$wrangled), fontSize = "13px")
+      })
+
+      ### Download data ----
+      #### Add download button into the UI ----
+      output$download_wrangled_data <- shiny::renderUI({
+        shiny::req(dataset$wrangled)
+        shiny::req(!dataset$wrangling())
+        htmltools::tags$div(
+          style = "margin-bottom: 15px; text-align: right;",
+          shiny::downloadButton(
+            outputId = ns("download_data"),
+            label = "Download Wrangled Data",
+            class = "btn-primary",
+            icon = shiny::icon(name = "download", class = "fa-lg")
+          )
+        )
+      })
+
+      #### Downloadable results by clicking on the download button ----
+      output$download_data <- shiny::downloadHandler(
+        filename = function() {
+          if (input$wrangle == "wfhz") {
+            paste0("mwana-wranged-data-wfhz_", Sys.Date(), ".xlsx", sep = "")
+          } else if (input$wrangle == "mfaz") {
+            paste0("mwana-wranged-data-mfaz_", Sys.Date(), ".xlsx", sep = "")
+          } else if (input$wrangle == "muac") {
+            paste0("mwana-wranged-data-muac_", Sys.Date(), ".xlsx", sep = "")
+          } else {
+            paste0("mwana-wranged-data-combined_", Sys.Date(), ".xlsx", sep = "")
+          }
+        },
+        content = function(file) {
+          shiny::req(dataset$wrangled) # Ensure results exist
+          tryCatch(
+            {
+              openxlsx::write.xlsx(dataset$wrangled, file)
+              shiny::showNotification("File downloaded successfully!", type = "message")
+            },
+            error = function(e) {
+              shiny::showNotification(paste("Error creating file:", e$message), type = "error")
+            }
+          )
+        }
+      )
+
+      #### Return data ----
+      return(shiny::reactive(dataset$wrangled))
+    }
+  )
+}
